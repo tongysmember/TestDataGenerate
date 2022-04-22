8dca0367a7295d66415bba0d2bd7a77b561375a4
tongysmember <tongysmember@hotmail.com>
2022-04-22 11:07:08 +0800

    [Update] Check User Input and Goto Function

--- TestDataGenerate.bat
+++ TestDataGenerate.bat
@@ -2,23 +2,52 @@
 echo 測試資料產生器
 echo ===============================================================
 
-set /p DdlPath=請輸入來源DDL檔案路徑(DDL資料夾當中檔案):
-echo %DdlPath%
+:CheckDdlFile
+	set /p DdlPath=請輸入來源DDL檔案路徑(DDL資料夾當中檔案):
+	set "DdlPath=./DDL/%DdlPath%"
+	echo %DdlPath%
 
-IF EXIST %DdlPath% (
-  echo 檔案存在
-) ELSE (
-  echo 檔案不存在, 請重新輸入
-)
 
-set /p BoolHeader=是否需要表頭Header(Y, N):
-echo %BoolHeader%
+:CheckHeader
+	set /p BoolHeader=是否需要表頭Header[Y/N]:
+	echo %BoolHeader%
+	set BoolHeaderInput=False
 
-set /p BoolTailer=是否需要表尾Tailer(Y, N):
-echo %BoolTailer%
+	IF %BoolHeader% == Y set "BoolHeaderInput=True"
+	IF %BoolHeader% == y set "BoolHeaderInput=True"
+	IF %BoolHeader% == N set "BoolHeaderInput=True"
+	IF %BoolHeader% == n set "BoolHeaderInput=True"
+	IF NOT %BoolHeaderInput%==True (
+		ECHO 請輸入[Y/N]
+		GOTO CheckHeader
+	)
+:CheckTailer
+	set /p BoolTailer=是否需要表尾Tailer[Y/N]:
+	echo %BoolTailer%
+	set BoolTailerInput=False
 
-set /p IntRows=資料產生筆數(正整數):
-echo %IntRows%
+	IF %BoolTailer% == Y set "BoolTailerInput=True"
+	IF %BoolTailer% == y set "BoolTailerInput=True"
+	IF %BoolTailer% == N set "BoolTailerInput=True"
+	IF %BoolTailer% == n set "BoolTailerInput=True"
+	IF NOT %BoolTailerInput%==True (
+		ECHO 請輸入[Y/N]
+		GOTO CheckTailer
+	)
+
+:CheckCNT
+	set /p IntRows=資料產生筆數(正整數):
+	echo %IntRows%
+	set BoolIntRowsInput=False
+	
+	SET "var="&for /f "delims=0123456789" %%i in ("%IntRows%") do set var=%%i
+	if defined var (set "BoolIntRowsInput=False") else (set "BoolIntRowsInput=True")
+
+	IF NOT %BoolIntRowsInput%==True (
+		ECHO 請輸入大於 0 整數
+		GOTO CheckCNT
+	)
+
 
 echo 執行程式 
 
